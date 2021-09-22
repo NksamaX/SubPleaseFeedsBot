e01d5b5d5cf78ec0bebac6f1c49ffacb85f2743a
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-09-22 09:45:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,82 @@
+
+from requests import get
+from pyrogram import Client , filters
+import os
+import time
+
+bot = Client(
+    ":memory:",
+    api_id=os.environ.get("API_ID"),
+    api_hash=os.environ.get("API_HASH"),
+    bot_token=os.environ.get("TOKEN")
+)
+
+CHAT_ID = os.environ.get('CHAT_ID')
+
+
+
+db = {
+    "latest": None
+}
+
+# fake db
+
+
+kek = []
+
+
+
+
+async def feeds():
+    async with bot:
+        while True:
+
+            url = 'https://subsplease.org/api/?f=latest&tz=Canada/central'
+            res = get(url).json()
+
+            k = None
+            for x in res:
+                kek.append(x)
+
+
+            anime_name = res[kek[0]]['show']
+            ep = res[kek[0]]['episode']
+
+
+
+
+            lnk = res[kek[0]]['downloads']
+            
+            last = db['latest'] or ""
+            
+            if last != anime_name:
+
+            
+
+                for x in lnk:
+                    quality = x['res']
+                    links = x['magnet']
+
+                    data = f"{quality}: ```{links}```\n\n"
+
+                    if k:
+                        k = f"{k}\n{data}"
+
+                    else:
+                        k = data
+
+                db['latest'] = anime_name
+                  await bot.send_message(CHAT_ID , f"**{anime_name}**:\n\n{k}" , parse_mode="markdown")
+                print(db)
+                time.sleep(300)
+
+    
+    
+    
+
+    
+bot.run(feeds())
+
+
+
+
